98701b3011bf3344e1bb73593f7ae45de851ad94
Courtney Remekie <courtney.remekie@gmail.com>
2026-05-11 16:42:31 -0600

    fix: html block use innerHTML not textContent

--- blocks/html/html.js
+++ blocks/html/html.js
@@ -2,6 +2,6 @@ export default function decorate(block) {
   const inner = block.querySelector(':scope > div > div');
   if (!inner) return;
   const wrapper = document.createElement('div');
-  wrapper.innerHTML = inner.textContent;
+  wrapper.innerHTML = inner.innerHTML;
   block.replaceWith(wrapper);
 }
